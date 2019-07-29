e20bf87fa0f8aa5455e20b8c6d9749b68e2f37ac
Niekta452 <nik_shah_2000@yahoo.com>
2019-07-29 22:48:36 +0100

    git add 'PersonalDetails' git commit -m 'first commit' git remote add origin https://github.com/Niketa452/personaldetails.git git push -u origin master

---
+++ src/PersonalDetails.java
@@ -0,0 +1,20 @@
+
+//my personal details are confidential;
+public class PersonalDetails {;
+public static void main (String  [] args ){;
+String name = ("Name: Niketa Parekh");
+String age = ("Age: 38");
+String fulladdress = ("Address: 10 Richmond Road. London SW5 7ML");
+        String mobile = ("Mobile: 07458861240");
+String email = ("Email ID: niketap@gmail.com");
+String salary = ("Salary: $50000");
+System.out.println (name);
+System.out.println (age);
+System.out.println (fulladdress);
+System.out.println (mobile);
+System.out.println (email);
+System.out.println (salary);
+    }
+}
+
+
